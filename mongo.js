e4929ff0c1104b14d342c64bcd99d1a8be5273c6
Adnan Mahmud <11adnan@live.com>
2023-01-19 02:28:12 +0600

    L 11

---
+++ mongo.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const uri = "mongodb://localhost:27017/productfinder";
+
+const options = {
+  autoIndex: false, // Don't build indexes
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // Use IPv4, skip trying IPv6
+};
+const connectWithDb = () => {
+  mongoose.set("strictQuery", true).connect(uri, options, (err, db) => {
+    if (err) {
+      console.error(err);
+    } else console.log("Connection established");
+  });
+};
+
+export default connectWithDb;
---
+++ services/userService.js
@@ -0,0 +1,16 @@
+import models from "../models";
+
+export const saveUser = async (user) => {
+  const model = new models.User({
+    username: user.username,
+    createdAt: new Date(),
+  });
+  const saveUser = await model.save();
+  return saveUser;
+};
+
+export const getAllUsers = async () => {
+  const User = models.User;
+  const users = await User.find();
+  return users;
+};
