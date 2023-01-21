7847d7682e49cc404f832efd77e1c49d0cb1f1ac
Adnan Mahmud <11adnan@live.com>
2023-01-22 01:04:39 +0600

    Tracking id added

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import express from "express";
 import configure from "./controllers";
 import connectWithDb from "./mongo";
-import { handleErrors } from './middlewares/handleErrors';
+import { handleErrors } from "./middlewares/handleErrors";
 
 const port = 3000;
 const app = express();
 
 app.use(express.json());
 
+const processRequest = async (req, res, next) => {
+  let correlationId = req.header["x-correlationId"];
+  if (!correlationId) {
+    correlationId = Date.now().toString();
+    req.header["x-correlationId"] = correlationId;
+  }
+  res.set('x-correlationId', correlationId);
+  return next();
+};
+
+app.use(processRequest);
 connectWithDb();
 
 configure(app);
@@ -17,7 +28,6 @@ app.use(handleErrors);
 app.listen(port, () => {
   console.log(port);
 });
-
 
 // 3 layer architechture
 //  1. userController = Controller Layer: process the http resquests
