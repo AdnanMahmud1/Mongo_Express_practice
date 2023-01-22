0a416b1730d034d9d8d1ca33ea69f0d7a9954acf
Adnan <Adnan@DESKTOP-UGPNVH0>
2023-01-22 16:47:29 +0600

    Test code runs successfully

--- app.test.js
+++ app.test.js
@@ -1,5 +1,33 @@
+import request from "supertest";
 import app from "./app";
 
-test('should work', async () =>{
+beforeAll(async () => {
+  console.log("before all");
+});
+
+afterAll(async () => {
+  console.log("after all");
+});
+
+beforeEach(async () => {
+  console.log("before each");
+});
+
+afterEach(async () => {
+  console.log("after each");
+});
+
+describe("user controller test suite", () => {
+  test("should work", async () => {
     console.log("first test");
-})
+  });
+
+  test("get all user should return all users", async () => {
+    console.log("get all user test");
+    let response = await request(app).get("/users");
+
+    expect(response.body).not.toBeNull();
+    expect(response.status).toBe(200);
+    console.log("🚀 ~ file: app.test.js:30 ~ test ~ response", response.body);
+  });
+});
