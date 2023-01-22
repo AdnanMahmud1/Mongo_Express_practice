e2bec640991e1d964841b2b790a540b10076d655
Adnan <Adnan@DESKTOP-UGPNVH0>
2023-01-22 13:04:24 +0600

    Errol logging in File system

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from "express";
 import configure from "./controllers";
 import connectWithDb from "./mongo";
 import { handleErrors } from "./middlewares/handleErrors";
+import winston from "winston";
+import expressWinston from "express-winston";
+import "winston-daily-rotate-file"
+// import winstonMongo from "winston-mongodb"
+// import {ElasticsearchTransport} from "winston-elasticsearch";
 
 const port = 3000;
 const app = express();
@@ -9,19 +14,59 @@ const app = express();
 app.use(express.json());
 
 const processRequest = async (req, res, next) => {
-  let correlationId = req.header["x-correlationId"];
+  let correlationId = req.header["x-correlation-id"];
   if (!correlationId) {
     correlationId = Date.now().toString();
-    req.header["x-correlationId"] = correlationId;
+    req.header["x-correlation-id"] = correlationId;
   }
-  res.set('x-correlationId', correlationId);
+  res.set("x-correlation-id", correlationId);
   return next();
 };
 
 app.use(processRequest);
+
 connectWithDb();
 
+const getMessage = (req, res) => {
+  let obj = {
+    correlationId: req.header["x-correlation-id"],
+    reqruestBody: req.body,
+  };
+  return JSON.stringify(obj);
+};
+
+const fileInfoTransport = new (winston.transports.DailyRotateFile)({
+  filename: "log-info-%DATE%.log",
+  datePattern: "yyyy-MM-DD-HH",
+});
+
+const fileErrorTransport = new (winston.transports.DailyRotateFile)({
+  filename: "log-error-%DATE%.log",
+  datePattern: "yyyy-MM-DD-HH",
+});
+
+const infoLogger = expressWinston.logger({
+  transports: [new winston.transports.Console(), fileInfoTransport],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: true,
+  msg: getMessage,
+});
+
+const errorLogger = expressWinston.errorLogger({
+  transports:[
+    new winston.transports.Console(),
+    fileErrorTransport
+  ]
+})
+
+app.use(infoLogger);
+
 configure(app);
+
+app.use(errorLogger);
 
 app.use(handleErrors);
 
--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,13 +1,14 @@
 import { GeneralError } from "./../utils/errors";
 
 export const handleErrors = async (err, req, res, next) => {
+  let code = 500;
   if (err instanceof GeneralError) {
-    const code = err.getCode();
-    return res.status(code).json({ name: err.name, message: err.message });
+    code = err.getCode();
+    //return res.status(code).json({ name: err.name, message: err.message });
   }
-  // no known error
-  return res.status(500).json({
-    name: "Internal server error",
+  let correlationId = req.header["x-correlation-id"];
+  return res.status(code).json({
+    correlationId: correlationId,
     message: err.message,
   });
 };
